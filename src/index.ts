90b533722753cb6ee2ff963d7f47051feac4cc16
Brendon Machado <benmacario.s@gmail.com>
2022-01-26 16:09:01 -0300

    docs: finish documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export { app };
